6e4ac917bd94f1900326675f58c433959606d99f
Andersen Huey <35656323+AHueya@users.noreply.github.com>
2020-12-15 15:44:46 -0800

    Update Integrated_Sensor_Code.ino

--- Integrated_Sensor_Code.ino
+++ Integrated_Sensor_Code.ino
@@ -9,20 +9,19 @@ int CALIBRATION_SAMPLE_INTERVAL=500;
 int READ_SAMPLE_INTERVAL=50;                        
 int READ_SAMPLE_TIMES=5;                            
                                                     
-#define         GAS_LPG             0   
-#define         GAS_CO              1   
-#define         GAS_SMOKE           2    
+#define GAS_LPG     0   
+#define GAS_CO      1   
+#define GAS_SMOKE   2    
 
-float           LPGCurve[3]   = {2.3,0.21,-0.47};   
+float LPGCurve[3]   = {2.3,0.21,-0.47};   
  
-float           COCurve[3]    = {2.3,0.72,-0.34};    
+float COCurve[3]    = {2.3,0.72,-0.34};    
  
-float           SmokeCurve[3] = {2.3,0.53,-0.44};   // two points are taken from the curve. 
-                                                    // with these two points, a line is formed which is "approximately equivalent" 
-                                                    // to the original curve.
-                                                    // data format:{ x, y, slope}; point1: (lg200, 0.53), point2: (lg10000,  -0.22)                                                     
-float           Ro            = 10;                 // Ro is initialized to 10 kilo ohms          
-
+float SmokeCurve[3] = {2.3,0.53,-0.44};     // two points are taken from the curve. 
+                                            // with these two points, a line is formed which is "approximately equivalent" 
+                                            // to the original curve.
+                                            // data format:{ x, y, slope}; point1: (lg200, 0.53), point2: (lg10000,  -0.22)                                                     
+float Ro = 10;                              // Ro is initialized to 10 kilo ohms          
 
 #define Analog_Input A0
 #define CO2_Zero_Ref 55
@@ -40,24 +39,23 @@ const int Analog_Pin = A5;
 // Initialize DHT sensor:
 DHT dht = DHT(DHTPIN, DHTTYPE);
 
-void setup() 
-{
-  Serial.begin(9600);
-  pinMode(Analog_Input, INPUT);
+void setup() {
+    Serial.begin(9600);
+    pinMode(Analog_Input, INPUT);
   
-  pinMode(calibrationLed,OUTPUT);
-  digitalWrite(calibrationLed,HIGH);                        
+    pinMode(calibrationLed,OUTPUT);
+    digitalWrite(calibrationLed,HIGH);                        
   
-  Ro = MQCalibration(MQ_PIN);                                  
-  digitalWrite(calibrationLed,LOW);              
+    Ro = MQCalibration(MQ_PIN);                                  
+    digitalWrite(calibrationLed,LOW);              
                                    
-  delay(3000);
+    delay(3000);
 
-  dht.begin();
-  delay(2000);
+    dht.begin();
+    delay(2000);
 }
-void loop() 
-{                               
+
+void loop() {                               
     int CO2_Conv_PPM = 0;                               
     float Vout = 0;
     long iPPM_LPG = 0;
@@ -69,14 +67,14 @@ void loop()
     //---- Serial Print --------------------------
     //Serial.println(readO2Level());
     //--------------------------------------------
-    delay(500);
+    delay(10);
 
     CO2_Conv_PPM = read_CO2_Vout();
     
     //---- Serial Print --------------------------
     //Serial.println(CO2_Conv_PPM);
     //--------------------------------------------
-    delay(100);
+    delay(10);
 
     iPPM_LPG = MQGetGasPercentage(MQRead(MQ_PIN)/Ro,GAS_LPG);
     iPPM_CO = MQGetGasPercentage(MQRead(MQ_PIN)/Ro,GAS_CO);
@@ -90,7 +88,7 @@ void loop()
     //Serial.print(iPPM_Smoke);
     //--------------------------------------------
 
-    delay(200);
+    delay(10);
  
     float h = dht.readHumidity();
     float t = dht.readTemperature();
@@ -117,8 +115,7 @@ void loop()
     
 }
 
-float read_O2_Vout()
-{
+float read_O2_Vout() {
     long sum = 0;
     for(int i=0; i<38; i++)
     {
@@ -129,16 +126,14 @@ float read_O2_Vout()
     return MeasuredVout;
 }   // Remember that analog read only reads up to 1023
 
-float readO2Level()
-{
+float readO2Level() {
     float MeasuredVout = read_O2_Vout(); 
     float Concentration = MeasuredVout * 0.21 / 2.0; 
     float Concentration_Percentage=Concentration*100;
     return Concentration_Percentage;
 }
 
-float read_CO2_Vout()
-{
+float read_CO2_Vout() {
     int Current_CO2_Level[10];                            
     int Raw_CO2_Level = 0;
     int CO2_PPM = 0;
@@ -147,7 +142,7 @@ float read_CO2_Vout()
     for (int x = 0;x<10;x++)  // sample CO2 10x over 2 seconds
     {                   
       Current_CO2_Level[x]=analogRead(A0);
-      delay(100);
+      delay(10);
     }
 
     for (int x = 0;x<10;x++) 
@@ -160,55 +155,51 @@ float read_CO2_Vout()
     return CO2_PPM;   
 }
  
-float MQResistanceCalculation(int raw_adc)
-{
+float MQResistanceCalculation(int raw_adc) {
   return ( ((float)RL_VALUE*(1023-raw_adc)/raw_adc));
 }
  
-float MQCalibration(int mq_pin)
-{
-  int i;
-  float val=0;
+float MQCalibration(int mq_pin) {
+    int i;
+    float val=0;
 
-  for (i=0;i<CALIBARAION_SAMPLE_TIMES;i++) {            //take multiple samples
-    val += MQResistanceCalculation(analogRead(mq_pin));
-    delay(CALIBRATION_SAMPLE_INTERVAL);
-  }
-  val = val/CALIBARAION_SAMPLE_TIMES;                   //calculate the average value
-  val = val/RO_CLEAN_AIR_FACTOR;                        //divided by RO_CLEAN_AIR_FACTOR yields the Ro                                        
-  return val;                                                      //according to the chart in the datasheet 
+    for (i=0;i<CALIBARAION_SAMPLE_TIMES;i++) {            //take multiple samples
+        val += MQResistanceCalculation(analogRead(mq_pin));
+        delay(CALIBRATION_SAMPLE_INTERVAL);
+    }
+    val = val/CALIBARAION_SAMPLE_TIMES;                   //calculate the average value
+    val = val/RO_CLEAN_AIR_FACTOR;                        //divided by RO_CLEAN_AIR_FACTOR yields the Ro                                        
+    return val;                                                      //according to the chart in the datasheet 
 
 }
  
-float MQRead(int mq_pin)
-{
-  int i;
-  float rs=0;
+float MQRead(int mq_pin) {
+    int i;
+    float rs=0;
  
-  for (i=0;i<READ_SAMPLE_TIMES;i++) {
-    rs += MQResistanceCalculation(analogRead(mq_pin));
-    delay(READ_SAMPLE_INTERVAL);
-  }
+    for (i=0;i<READ_SAMPLE_TIMES;i++) {
+        rs += MQResistanceCalculation(analogRead(mq_pin));
+        delay(READ_SAMPLE_INTERVAL);
+    }
  
-  rs = rs/READ_SAMPLE_TIMES;
-  return rs;  
+    rs = rs/READ_SAMPLE_TIMES;
+    return rs;  
 }
  
 
-long MQGetGasPercentage(float rs_ro_ratio, int gas_id)
-{
-  if ( gas_id == GAS_LPG ) {
-     return MQGetPercentage(rs_ro_ratio,LPGCurve);
-  } else if ( gas_id == GAS_CO ) {
-     return MQGetPercentage(rs_ro_ratio,COCurve);
-  } else if ( gas_id == GAS_SMOKE ) {
-     return MQGetPercentage(rs_ro_ratio,SmokeCurve);
-  }    
+long MQGetGasPercentage(float rs_ro_ratio, int gas_id) {
+    if ( gas_id == GAS_LPG ) {
+        return MQGetPercentage(rs_ro_ratio,LPGCurve);
+    } else if ( gas_id == GAS_CO ) {
+        return MQGetPercentage(rs_ro_ratio,COCurve);
+    } else if ( gas_id == GAS_SMOKE ) {
+        return MQGetPercentage(rs_ro_ratio,SmokeCurve);
+    }    
  
-  return 0;
+    return 0;
 }
  
-long  MQGetPercentage(float rs_ro_ratio, float *pcurve)
-{
-  return (pow(10,( ((log(rs_ro_ratio)-pcurve[1])/pcurve[2]) + pcurve[0])));
+long  MQGetPercentage(float rs_ro_ratio, float *pcurve) {
+    return (pow(10,( ((log(rs_ro_ratio)-pcurve[1])/pcurve[2]) + pcurve[0])));
 }
+
